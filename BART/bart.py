ccdcb2dbfac5f867c68b1e810dca5ed64b57b79e
ANU PANDEY <dr.anupandey.86@gmail.com>
2017-09-27 23:37:51 -0700

    BART get_station_name_dict

--- BART/bart.py
+++ BART/bart.py
@@ -8,50 +8,62 @@ class BartApi(object):
         self.api_prefix = 'https://api.bart.gov/api'
 
     # This can be used to find all stations and corresponding abbr (required for page request)
-    def get_station_name_list(self):
+    def get_station_name_dict(self):
         station_list_page = '%s/stn.aspx?cmd=stns&key=%s&json=y' % (self.api_prefix, self.api_key)
-        r = requests.get(station_list_page)
-        payload_dict = json.loads(r.text)
+        response = requests.get(station_list_page)
+
+        # This raises a ValueError if status_code is not 200
+        if response.status_code != 200:
+            raise ValueError(
+                "There is some problem with API - Check whether page %s is accessible" % (station_list_page))
+
+        payload_dict = json.loads(response.text)
+        # json output returns a dictionary - which comprises of a list of stations which is further comprised of dict
         station_list = payload_dict['root']['stations']['station']
 
-        station_dict = {}
+        station_name_abbr_dict = {}
         '''
-            For now, this is of the type
+            For now, this dictionary is of the type
             {
                 "Union City" : "UCTY"
             }
         '''
 
-        station_abbr_dict = {}
+        station_abbr_name_dict = {}
         '''
-            For now, this is of the type
+            For now, this dictionary is of the type
             {
                 "UCTY" : "Union City"
             }
         '''
 
-        for station in station_list:
-            key = station['name']
+        for station_dict in station_list:
+            # note- station_dict is a dict which is a part of list - station_list
+            name = station_dict['name']
 
             # Found a bug in the api where stn name is sometimes Warm Springs
             # and sometimes Warm Springs / South Fremont. We might need to change
             # this if BART fixes it
-            if 'Warm' in key:
-                key = 'Warm Springs'
+            if 'Warm' in name:
+                name = 'Warm Springs'
 
-            value = station['abbr']
-            station_dict[key] = value
+            abbr = station_dict['abbr']
 
-            station_abbr_dict[value] = key
+            station_name_abbr_dict[name] = abbr
+            station_abbr_name_dict[abbr] = name
 
-        return station_dict, station_abbr_dict
+        return station_name_abbr_dict, station_abbr_name_dict
 
 
 bart_api = BartApi()
-station_dict, station_abbr_dict = bart_api.get_station_name_list()
+
+station_name_abbr_dict, station_abbr_name_dict = bart_api.get_station_name_dict()
 
 print('List of all BART stations')
-for key, value in station_dict.items():
+for key, value in station_name_abbr_dict.items():
     print("%40s: %s" % (key, value))
 
 print('\n')
+
+for key, value in station_abbr_name_dict.items():
+    print("%s: %40s" % (key, value))
