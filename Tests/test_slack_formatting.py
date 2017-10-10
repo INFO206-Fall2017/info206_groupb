ed35c2417025c15dd054ac23f5ae357b745e9b2e
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-10-10 12:03:12 -0700

    Added slack formatting testcase

---
+++ Tests/test_slack_formatting.py
@@ -0,0 +1,51 @@
+import unittest
+import requests
+import json
+import Slackbot
+
+from Slackbot import *
+from Slackbot.intent_responder import BARTQueryResponse, BusQueryResponse
+
+# curl -X POST -H 'Content-type: application/json' \
+# --data '{"text":"Thisss is a line of text.\nAnd this is another one."}' \
+#  https://hooks.slack.com/services/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
+
+class SlackFormatterTest(unittest.TestCase):
+    def test(self):
+      webhook_url = 'https://hooks.slack.com/services/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'
+      headers = { 'Content-type': 'application/json' }
+      # message = {
+      #   "text": "Thisss is a line of text from Python.\nAnd this is another one."
+      # }
+
+      message = {
+        "attachments": [
+          {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+              {
+                "title": "Priority",
+                "value": "High",
+                "short": False
+              }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+          }
+        ]
+      }
+      r = requests.post(webhook_url, data=json.dumps(message), headers=headers)
+
+if __name__ == '__main__':
+    unittest.main()
