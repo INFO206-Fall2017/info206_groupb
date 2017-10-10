ed4b5e7461e9091d4509855a46f0376db501ed54
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-10-10 12:17:26 -0700

    Added slack output in message formatter test case

--- Tests/test_slack_formatting.py
+++ Tests/test_slack_formatting.py
@@ -42,6 +42,29 @@ class SlackFormatterTest(unittest.TestCase):
             "footer": "Slack API",
             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
             "ts": 123456789
+          },
+          {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "red",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment 2",
+            "fields": [
+              {
+                "title": "Priority",
+                "value": "High",
+                "short": False
+              }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
           }
         ]
       }
